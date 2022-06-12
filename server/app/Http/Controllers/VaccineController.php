fb410a99ecd10150a2607213b4393de0999e8e93
Victo <victor.barra@student.howest.be>
2022-06-12 15:39:09 +0200

    added controller

---
+++ server/app/Http/Controllers/VaccineController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VaccineController extends Controller
+{
+    function test(){
+        return ["message" => "test"];
+    }
+}
